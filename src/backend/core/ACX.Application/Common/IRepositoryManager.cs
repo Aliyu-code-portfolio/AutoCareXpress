9d5620bcdda49b226296cf1bb2726b144c4a9e7b
Dee-Joshua <officialdariajoshua@gmail.com>
2023-06-30 16:37:02 +0100

    IRepositoryManger Fix

--- src/backend/core/ACX.Application/Common/IRepositoryManager.cs
+++ src/backend/core/ACX.Application/Common/IRepositoryManager.cs
@@ -9,10 +9,7 @@ namespace ACX.Application.Common
 {
     public interface IRepositoryManager
     {
-        public IUserRepository UserRepository { get; }
-        public IAppointmentRepository AppointmentRepository { get; }
-        public IServiceProviderRepository ServiceProviderRepository { get; }
-        public IProviderServiceRepository ProviderServiceRepository { get; }
+
         public IRef_Service_Type_Repository IRef_Service_Type_Repository { get;}
         public IRef_Service_Location_Repository IRef_Service_Location_Repository { get; }  
     }
