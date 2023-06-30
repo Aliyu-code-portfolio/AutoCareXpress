c9586ac47e68378600305fefa6e112107547cca5
Joyce-Adesina <joycegabriels32@gmail.com>
2023-06-30 14:44:08 +0100

    Just added on the domain layer Vehicle and PickUpAddress And Application layer IVehicles and IPickUpAddress

---
+++ src/backend/core/ACX.Application/Contract/IPickUpAddress_Repository.cs
@@ -0,0 +1,19 @@
+﻿using ACX.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACX.Application.Contract
+{
+    public interface IPickUpAddress_Repository
+    {
+        Task<IEnumerable<PickUpAddress>> GetAllPickUpPickUpAddress(bool trackChanges);
+        Task<PickUpAddress> GetPickUpAddressById(int id, bool trackChanges);
+        void AddPickUpAddress(PickUpAddress pickupaddress);
+        Task UpdatePickUpAddress(PickUpAddress pickupaddress);
+        void DeletePickUpAddress(PickUpAddress pickupAddress);
+    }
+}
--- src/backend/core/ACX.Domain/Model/Vehicle.cs
+++ src/backend/core/ACX.Domain/Model/Vehicle.cs
@@ -1,10 +1,38 @@
 ﻿using ACX.Domain.Common;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ACX.Domain.Model
 {
-    public class Vehicle:AuditableBaseEntity
+    public class Vehicle : AuditableBaseEntity
     {
 
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Id))]
+        public Guid UserId { get; set; }
+
+        [Required(ErrorMessage = "Name Description is required")]
+        [StringLength(50, ErrorMessage = "Name Description must be at most 50 characters")]
+        public string? Name { get; set; }
+
+     
+        public string? Color { get; set; }
+
+        [Required(ErrorMessage = "PlateNumber is required")]
+        [StringLength (10, ErrorMessage = "PlateNumber must be at most 10 alphanumeric Characters")]
+        public string? PlateNumber { get; set; }
+
+        [Required(ErrorMessage = "Manufacture Name is required")]
+        [StringLength(25, ErrorMessage = "Manufacture Name must be at most 25 characters")]
+        public string? Manufacture { get; set; }
+
+        [Required(ErrorMessage = "Model Name is required")]
+        [StringLength(25, ErrorMessage = "Model Name must be at most 25 characters")]
+        public string? Model { get; set; }
+
+        // Nagivational Properties
+       
     }
 }
