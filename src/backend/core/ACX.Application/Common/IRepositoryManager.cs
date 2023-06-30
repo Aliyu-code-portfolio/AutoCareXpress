be077bdf13fbb014b5c37f4f3adaf7e48822aef5
Dee-Joshua <officialdariajoshua@gmail.com>
2023-06-30 16:41:29 +0100

    IRepositoryManger Completed

--- src/backend/core/ACX.Application/Common/IRepositoryManager.cs
+++ src/backend/core/ACX.Application/Common/IRepositoryManager.cs
@@ -9,8 +9,13 @@ namespace ACX.Application.Common
 {
     public interface IRepositoryManager
     {
+        public IUserRepository UserRepository { get; }
+        public IAppointmentRepository AppointmentRepository { get; }
+        public IServiceProviderRepository ServiceProviderRepository { get; }
+        public IProviderServiceRepository ProviderServiceRepository { get; }
         public IPickUpAddress_Repository PickUpAddress_Repository { get; }
         public IVehicle_Repository Vehicle_Repository { get; }
         public IRef_Service_Type_Repository Ref_Service_Type_Repository { get;}
-        public IRef_Service_Location_Repository Ref_Service_Location_Repository { get; }  }
+        public IRef_Service_Location_Repository Ref_Service_Location_Repository { get; }  
+    }
 }
